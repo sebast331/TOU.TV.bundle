8f0681b0d9facf5a15c7e16ff9f0a4921eae3cef
Mike M <mike.d.meier@gmail.com>
2012-06-09 12:50:43 -0600

    release ready

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -186,7 +186,8 @@ def Season(show_title, season, season_name):
 		except:
 			ep_index = None
 		if title.startswith(u"Épisode"):
-			title = str(title).partition(':')[2]
+			if title.split(':')[1] != '':
+				title = str(title).partition(':')[2]
 		date = TranslateDate(episode["date"])
 		summary = episode["summary"]
 		thumb = episode["thumb"]
