b4352942018d229f74714993f39c85a5cc3b32ff
Sébastien Huneault <sebast331@gmail.com>
2014-07-19 22:19:12 -0400

    Added categories and search in browsing

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,3 +1,6 @@
+# import
+import re
+
 # Plugin Preferences
 PLUGIN_NAME             = "ICI TOU.TV"
 PLUGIN_TITLE            = "TOU.TV"
@@ -7,6 +10,7 @@ PLUGIN_PREFIX           = "/video/TOU.TV"
 URL_CARROUSEL           = "http://api.tou.tv/v1/toutvapiservice.svc/json/GetCarrousel?playlistName=Carrousel%20Accueil"
 URL_ALL_SHOWS           = "http://api.tou.tv/v1/toutvapiservice.svc/json/GetPageRepertoire"
 URL_SHOW                = "http://api.tou.tv/v1/toutvapiservice.svc/json/GetPageEmission?emissionId=%s"
+URL_ACCUEIL				= "http://api.tou.tv/v1/toutvapiservice.svc/json/GetPageAccueil"
 
 # make sure to replace artwork with what you want
 # these filenames reference the example files in
@@ -30,28 +34,46 @@ def Start():
     ObjectContainer.title1 = PLUGIN_TITLE
     
     # Default HTTP
-    HTTP.CacheTime = CACHE_1HOUR
+    #HTTP.CacheTime = CACHE_1HOUR
+    HTTP.CacheTime = 0
 
 def MainMenu():
-    Log('Entering MainMenu')
+
     oc = ObjectContainer()
 
     # Main Menu
-    oc.add(DirectoryObject(key=Callback(AllShows), title=u"Toutes les émissions"))
+    oc.add(DirectoryObject(key=Callback(CarrouselMenu), title=u"En vedette"))
+    oc.add(DirectoryObject(key=Callback(ListShowsMenu, title=u"Toutes les émissions"), title=u"Toutes les émissions"))
+    oc.add(DirectoryObject(key=Callback(SelectionsMenu, sectionName="SelectionADecouvrir", sectionTitle=u"Sélections à découvrir"), title=u"Sélections à découvrir"))
+    oc.add(DirectoryObject(key=Callback(GenresListMenu), title=u"Parcourir par genre"))
+    oc.add(DirectoryObject(key=Callback(LetterListMenu), title=u"Parcourir par ordre alphabétique"))
+    # Search
+    oc.add(InputDirectoryObject(key=Callback(ListShowsMenu, title="Resultats de recherche: "), title="Search Title", summary="The description of what you are searching for..."))
+
  
     return oc
 
 ######################################################
-@route(PLUGIN_PREFIX + '/AllShows')
-def AllShows():
-    Log('Entering AllShows')
-    oc = ObjectContainer(title2 = u"Toutes les émissions")
+#	Displays the list of shows that matches the conditions
+@route(PLUGIN_PREFIX + '/ListShowsMenu')
+def ListShowsMenu(title, genre=None, titleRegex=None, query=""):
+
+    oc = ObjectContainer(title2 = title + query)
+
+    # Query is the result of a search and should be the regex
+    if (query != ""):
+    	titleRegex = query
 
     # Get the AllShows
     shows = JSON.ObjectFromURL(URL_ALL_SHOWS)['d']['Emissions']
     for show in shows:
-        thumb = Resource.ContentsOfURLWithFallback(url=show['ImageJorC'])
-        oc.add(DirectoryObject(key=Callback(ShowMenu, show=show), title = show['Titre'], thumb = thumb, art = thumb))
+    	if (
+    			(genre == None or genre == show['Genre'])
+    		and (titleRegex == None or re.compile(titleRegex, re.IGNORECASE).search(show['Titre']))
+    		):
+    		# Display the show that matches the conditions
+	        thumb = Resource.ContentsOfURLWithFallback(url=show['ImageJorC'])
+	        oc.add(DirectoryObject(key=Callback(ShowMenu, show=show), title = show['Titre'], thumb = thumb, art = thumb))
 
     return oc
 
@@ -61,7 +83,6 @@ def AllShows():
 
 def ShowMenu(show):
 
-    Log("Entering ShowMenu")
     oc = ObjectContainer(title2 = show['Titre'])
 
     # Get the objects
@@ -84,21 +105,22 @@ def ShowMenu(show):
 ##########################################
 #   Displays the list of shows
 ##########################################
-@route(PLUGIN_PREFIX + '/EpisodesMenu', showId=int, season=int)
-def EpisodesMenu(showId, season):
-    jsonShows = JSON.ObjectFromURL(URL_SHOW % str(showId))
+@route(PLUGIN_PREFIX + '/EpisodesMenu', showId=str, season=int)
+def EpisodesMenu(showId, season=-1):
+    jsonShows = JSON.ObjectFromURL(URL_SHOW % showId)
     jsonEmission = jsonShows['d']['Emission']
     jsonEpisodes = jsonShows['d']['Episodes']
 
     oc = ObjectContainer(title2 = "%s - Saison %s" % (jsonEmission['Title'], str(season)) )
 
     for show in jsonEpisodes:
-        if show['SeasonNumber'] == season:
+        if (show['SeasonNumber'] == season or season == -1):
             movieTitle = show['Title']
             movieSummary = show['Description']
             movieYear = int(show['Year'])
             movieUrl = show['Url'].lstrip('/')
             movieDuration = int(show['Length'])
+            movieId = show['TitleID']
             try:
                 movieThumb = show['ImageThumbMoyenL']
             except:
@@ -109,6 +131,78 @@ def EpisodesMenu(showId, season):
                 movieArt = None
 
             ### TODO: PLAY VIDEO
-            oc.add(MovieObject(key='a' + str(show['PID']), rating_key='b' + str(show['PID']), title=movieTitle, summary=movieSummary, year=movieYear, duration=movieDuration, thumb=Resource.ContentsOfURLWithFallback(url=movieThumb), art=Resource.ContentsOfURLWithFallback(url=movieArt)))
+            oc.add(MovieObject(key=movieId, rating_key=movieId, title=movieTitle, summary=movieSummary, year=movieYear, duration=movieDuration, thumb=Resource.ContentsOfURLWithFallback(url=movieThumb), art=Resource.ContentsOfURLWithFallback(url=movieArt)))
 
     return oc
+
+
+
+##########################################
+#   Displays the list of shows
+##########################################
+@route(PLUGIN_PREFIX + '/SelectionsMenu')
+def SelectionsMenu(sectionName, sectionTitle):
+    jsonShows = JSON.ObjectFromURL(URL_ACCUEIL)
+    jsonEpisodes = jsonShows['d'][sectionName.split('/')[0]]['Episodes']
+
+    oc = ObjectContainer(title2 = unicode(sectionTitle))
+
+    for show in jsonEpisodes:
+        movieTitle = show['Title']
+        movieSummary = show['Description']
+        movieYear = int(show['Year'])
+        movieUrl = show['Url'].lstrip('/')
+        movieDuration = int(show['Length'])
+        movieId = show['TitleID']
+        try:
+            movieThumb = show['ImageThumbMoyenL']
+        except:
+            movieThumb = None
+        try:
+            movieArt = show['ImagePromoLargeL']
+        except:
+            movieArt = None
+
+        ### TODO: PLAY VIDEO
+        oc.add(MovieObject(key=movieId, rating_key=movieId, title=movieTitle, summary=movieSummary, year=movieYear, duration=movieDuration, thumb=Resource.ContentsOfURLWithFallback(url=movieThumb), art=Resource.ContentsOfURLWithFallback(url=movieArt)))
+
+    return oc
+
+
+######################################
+@route(PLUGIN_PREFIX + '/GenresListMenu')
+def GenresListMenu():
+	json = JSON.ObjectFromURL(URL_ACCUEIL)
+	jsonGenres = json['d']['Genres']
+
+	oc = ObjectContainer(title2 = u"Parcourir par genre")
+
+	for genre in jsonGenres:
+		oc.add(DirectoryObject(key=Callback(ListShowsMenu, title=genre['Title'], genre=genre['Title']), title=genre['Title']))
+
+	return oc
+
+
+######################################
+@route(PLUGIN_PREFIX + '/CarrouselMenu')
+def CarrouselMenu():
+	json = JSON.ObjectFromURL(URL_CARROUSEL)
+	jsonCarrousel = json['d']
+
+	oc = ObjectContainer(title2 = u"En vedette")
+
+	for show in jsonCarrousel:
+		oc.add(MovieObject(key=show['EmissionId'], rating_key=show['EmissionId'], title=show['title'], summary=show['subTitle'], thumb=Resource.ContentsOfURLWithFallback(url=show['imgNR']), art=Resource.ContentsOfURLWithFallback(url=show['imgLR'])))
+
+	return oc
+
+
+######################################
+@route(PLUGIN_PREFIX + '/LetterListMenu')
+def LetterListMenu():
+	oc = ObjectContainer(title2 = u"Parcourir par ordre alphabétique")
+
+	for letters in ["0-9", "ABC", "DEF", "GHI", "JKL", "MNO", "PQR", "STU", "VWXYZ"]:
+		oc.add(DirectoryObject(key=Callback(ListShowsMenu, title=letters, titleRegex="^[%s]" % letters), title=letters))
+
+	return oc
